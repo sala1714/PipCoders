0f7bf3f01ee6c89fb5634cb800ed9444c95f7bbd
Josep Sala Marqués <53835258+sala1714@users.noreply.github.com>
2020-05-01 21:34:00 +0200

    Version_2.2_UpdateSort

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -34,12 +34,36 @@ def main():
 
         median_countries = dict()
         for x in list(countries.keys()):
-            median_countries[x] = median(sorted(countries[x]["Length"]))
+            median_countries[x] = median(countries[x]["Length"])
         final_median_dict(countries, median_countries)
 
 
 def median(l):
+    sort(l)
     return l[len(l) // 2]
+
+
+def sort(array):
+    """Sort the array by using quicksort."""
+
+    less = []
+    equal = []
+    greater = []
+
+    if len(array) > 1:
+        pivot = array[0]
+        for x in array:
+            if x < pivot:
+                less.append(x)
+            elif x == pivot:
+                equal.append(x)
+            elif x > pivot:
+                greater.append(x)
+        # Don't forget to return something!
+        return sort(less)+equal+sort(greater)  # Just use the + operator to join lists
+    # Note that you want equal ^^^^^ not pivot
+    else:  # You need to handle the part at the end of the recursion - when you only have one element in your array, just return the array.
+        return array
 
 
 def final_median_dict(countries, median_countries):
