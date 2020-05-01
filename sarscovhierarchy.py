b30700d64fc9ca54d15ea90649df2abce8614ea8
Josep Sala Marqués <53835258+sala1714@users.noreply.github.com>
2020-05-01 20:03:21 +0200

    Finalversion_1.0

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -1,5 +1,7 @@
 import csv
 import pandas as pd
+
+
 def main():
     with open('sequences.csv', newline='') as csvfile:
         data = csv.DictReader(csvfile, delimiter=";")
@@ -12,8 +14,8 @@ def main():
         list_accession = list()
         list_accession.append(str(first_row["Accession"]))
 
-        countries[location].update({"Accession":list_accession})
-        countries[location].update({"Length":list_len})
+        countries[location].update({"Accession": list_accession})
+        countries[location].update({"Length": list_len})
 
         for row in data:
             if location in row["Geo_Location"]:
@@ -34,24 +36,30 @@ def main():
                 countries[location].update({"Accession": list_accession})
                 countries[location].update({"Length": list_len})
 
-        print(countries)
         median_countries = dict()
         for x in list(countries.keys()):
             median_countries[x] = median(sorted(countries[x]["Length"]))
+        final_median_dict(countries, median_countries)
 
-        for x in list(countries.keys()):
-            if median_countries[x] not in countries[x]["Length"]:
-                print(x)
-                print(median_countries[x])
 
 def median(l):
     if len(l) % 2 == 0:
         n = len(l)
         mediana = (l[n // 2 - 1] + l[n // 2]) // 2
-        if l[n//2]-mediana < mediana-l[n//2-1]:
-            return l[n//2]
+        if l[n // 2] - mediana < mediana - l[n // 2 - 1]:
+            return l[n // 2]
         else:
-            return l[n//2-1]
+            return l[n // 2 - 1]
     else:
         return l[len(l) // 2]
+
+
+def final_median_dict(countries, median_countries):
+    result = dict()
+    for x in list(countries.keys()):
+        medianIn = countries[x]["Length"].index(median_countries[x])
+        result[x] = countries[x]["Accession"][medianIn]
+    print(result)
+
+
 main()
