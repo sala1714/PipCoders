932b5d0e8b80b9b77510c6e50d8d01215d09bf25
ilyassm00 <64154465+ilyassm00@users.noreply.github.com>
2020-05-10 14:01:00 +0200

    Version_4.0_ImplementsAligmentAlgorithm

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -8,8 +8,8 @@ import glob
 
 def main():
     dictionary_median_accessions = select_accessions()  # First the program obtains the median sample for each country.
-    fasta_RNA(dictionary_median_accessions)  # In second place, the program associates the RNA sequence with the sample.
-    # createDictionary(dictionary_median_accessions)
+    dictionary_with_fasta = fasta_RNA(dictionary_median_accessions)  # In second place, the program associates the RNA sequence with the sample.
+    createDictionary(dictionary_with_fasta)
 
 
 def select_accessions():
@@ -83,7 +83,7 @@ def fasta_RNA(dictionary):
             path = str(path[0])
         for file in SeqIO.parse(path, "fasta"):
             dictionary[country].update({"RNA": str(file.seq)[0:1000]})
-    print(pd.DataFrame(dictionary).T)
+    return dictionary
 
 
 def download_FASTA(accession):
@@ -95,12 +95,68 @@ def download_FASTA(accession):
 
 def createDictionary(dictionary):
     result = dict()
+    S = similarity_matrix()
     countries = list(dictionary.keys())
     for country in countries:
         result[country] = dict()
+        A = dictionary[country]["RNA"]
         for country2 in countries:
-            result[country].update({country2: ""})
-    print(pd.DataFrame(result).T)
+            B = dictionary[country2]["RNA"]
+            result[country].update({country2: maximum_score(S, A, B)})
+    pd.DataFrame(dictionary).to_csv('out.csv', index=False)
+
+
+def similarity_matrix():
+    matrix = dict()
+    for i in ["A", "G", "C", "T"]:
+        matrix[i] = dict()
+
+    matrix["A"].update({"A": 10})
+    matrix["A"].update({"G": -1})
+    matrix["A"].update({"C": -3})
+    matrix["A"].update({"T": -4})
+    matrix["G"].update({"A": -1})
+    matrix["G"].update({"G": 7})
+    matrix["G"].update({"C": -5})
+    matrix["G"].update({"T": -3})
+    matrix["C"].update({"A": -3})
+    matrix["C"].update({"G": -5})
+    matrix["C"].update({"C": 9})
+    matrix["C"].update({"T": 0})
+    matrix["T"].update({"A": -4})
+    matrix["T"].update({"G": -3})
+    matrix["T"].update({"C": 0})
+    matrix["T"].update({"T": 8})
+    return matrix
+
+
+def maximum_score(S, A, B):
+    d = -5
+    F = [[0 for x in range(len(A) + 1)] for y in range(len(B) + 1)]
+    for i in range(len(B) + 1):
+        F[i][0] = i * d
+    for j in range(len(A) + 1):
+        F[0][j] = j * d
+    for i in range(1, (len(B) + 1)):
+        for j in range(1, (len(A) + 1)):
+            if A[j - 1] == "N" and B[j - 1] == "N":
+                match = F[i - 1][j - 1] + S[random.choice(list(S.keys()))][random.choice(list(S.keys()))]
+            elif A[j - 1] == "N":
+                match = F[i - 1][j - 1] + S[random.choice(list(S.keys()))][B[i - 1]]
+            elif B[i - 1] == "N":
+                match = F[i - 1][j - 1] + S[A[j - 1]][random.choice(list(S.keys()))]
+            elif A[j - 1] == "K" and B[j - 1] == "K":
+                match = F[i - 1][j - 1] + S[random.choice(["G", "C"])][random.choice(["G", "C"])]
+            elif A[j - 1] == "K":
+                match = F[i - 1][j - 1] + S[random.choice(["G", "C"])][B[i - 1]]
+            elif B[i - 1] == "K":
+                match = F[i - 1][j - 1] + S[A[j - 1]][random.choice(["G", "C"])]
+            else:
+                match = F[i - 1][j - 1] + S[A[j - 1]][B[i - 1]]
+            delete = F[i - 1][j] + d
+            insert = F[i][j - 1] + d
+            F[i][j] = max(delete, insert, match)
+    return F[-1][-1]
 
 
 main()
