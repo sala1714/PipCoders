397e41a9d21afe4cb3b211e174d010ae3d496ee8
pabloalastomas <64154426+pabloalastomas@users.noreply.github.com>
2020-05-15 15:43:52 +0200

    //docstring sarscovhierarchy.py

--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -1,19 +1,17 @@
-from module1 import *
-from module2 import *
-from module3 import *
 import pandas as pd
+import module1 as m1
+import module2 as m2
 
 
-dictionary_median_accessions = select_accessions()  # First the program obtains the median sample for each country.
-dictionary_with_fasta = fasta_rna(dictionary_median_accessions)  # In second place, the program associates the RNA
-
-# sequence with the sample.
-result = create_dictionary(dictionary_with_fasta)
+# First the program obtains the median sample for each country.
+dictionary_median_accessions = m1.select_accessions()
+# In second place, the program associates the RNA
+dictionary_with_median_accessions = dictionary_median_accessions
+# sequence with the sample FASTA.
+dictionary_with_fasta = m2.fasta_rna(dictionary_with_median_accessions)
+# sequence with the sample aligments.
+result = m2.create_dictionary(dictionary_with_fasta)
 print(pd.DataFrame(result).T)
-distances_dictionary = distances(load_aligments(list(dictionary_with_fasta.keys())))
+# sequence with the sample distances bettween aligments.
+# distances_dictionary = distances(load_aligments(list(dictionary_with_fasta.keys())))
 # print(pd.DataFrame(distances_dictionary))
-
-
-
-
-
