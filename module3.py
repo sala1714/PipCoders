4671c9f63db8936d60aa9c13205a5e6428d64df3
ilyassm00 <64154465+ilyassm00@users.noreply.github.com>
2020-05-18 19:19:11 +0200

    Version_6.0_Module3

--- module3.py
+++ module3.py
@@ -1,1 +1,44 @@
-import matplotlib.pyplot as plot
+import random
+
+
+def clustering(distances, k):
+    countries = list(distances.keys())
+    centers = random.sample(countries, k)
+    last_centers = list()
+    clusters = dict()
+    while (centers!=last_centers):
+        clusters = dict()
+        for center in centers:
+            clusters[center] = dict()
+            clusters[center]["Cluster"]=list()
+        for country in countries:
+            center = get_center(country, centers, distances)
+            cluster_list = clusters[center]["Cluster"]
+            cluster_list.append(country)
+            clusters[center]["Cluster"] = cluster_list
+        sum_distances = dict()
+        for center in centers:
+            sum_distances[center] = list()
+            for country in clusters[center]["Cluster"]:
+                sum_result = 0
+                for country2 in clusters[center]["Cluster"]:
+                    sum_result += distances[country][country2]
+                sum_distances[center].append(sum_result)
+        last_centers = centers
+        centers = new_centers(sum_distances, clusters)
+    return clusters
+
+
+def new_centers(sum_distances, clusters):
+    centers = list()
+    for center in list(sum_distances.keys()):
+        mini = sum_distances[center].index(min(sum_distances[center]))
+        centers.append(clusters[center]["Cluster"][mini])
+    return centers
+
+
+def get_center(country, centers, distances):
+    distances_of_center = list()
+    for center in centers:
+        distances_of_center.append(distances[center][country])
+    return centers[distances_of_center.index(min(distances_of_center))]
--- sarscovhierarchy.py
+++ sarscovhierarchy.py
@@ -7,6 +7,7 @@
 import pandas as pd
 import module1 as m1
 import module2 as m2
+import module3 as m3
 
 # First the program obtains the median sample for each country.
 dictionary_median_accessions = m1.select_accessions()
@@ -15,7 +16,9 @@ dictionary_with_median_accessions = dictionary_median_accessions
 # sequence with the sample FASTA.
 dictionary_with_fasta = m2.fasta_rna(dictionary_with_median_accessions)
 # sequence with the sample alignments.
-result = m2.create_dictionary(dictionary_with_fasta)
+#result = m2.create_dictionary(dictionary_with_fasta)
 # sequence with the sample distances between alignments.
-# distances_dictionary = distances(load_alignments(list(dictionary_with_fasta.keys())))
-# print(pd.DataFrame(distances_dictionary))
+distances_dictionary = m2.distances(m2.load_alignments(list(dictionary_with_fasta.keys())))
+clusters = m3.clustering(distances_dictionary, 12)
+print(clusters)
+print(pd.DataFrame(clusters).T)
